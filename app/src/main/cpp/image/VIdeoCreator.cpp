2ad59549b841672722fd3964c26f32f5d4ed6580
Wei Chen <weichen@narvii.com>
2019-09-06 09:26:40 +0800

    add VideoCreator

---
+++ app/src/main/cpp/image/VIdeoCreator.cpp
@@ -0,0 +1,13 @@
+//
+// Created by templechen on 2019-09-06.
+//
+
+#include "VIdeoCreator.h"
+
+AVFrame *VIdeoCreator::readFrame(const char *path, int index) {
+    return nullptr;
+}
+
+void VIdeoCreator::releaseFrame() {
+
+}

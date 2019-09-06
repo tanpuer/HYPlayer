3b545b8b90e05d4be70bf00f925e6cf3438da28a
Wei Chen <weichen@narvii.com>
2019-09-06 19:25:11 +0800

    add libx264

---
+++ app/src/main/cpp/encode/FFmpegEncoder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by templechen on 2019-09-06.
+//
+
+#include "FFmpegEncoder.h"
---
+++ app/src/main/cpp/encode/FFmpegEncoder.h
@@ -0,0 +1,14 @@
+//
+// Created by templechen on 2019-09-06.
+//
+
+#ifndef HYPLAYER_FFMPEGENCODER_H
+#define HYPLAYER_FFMPEGENCODER_H
+
+
+class FFmpegEncoder {
+
+};
+
+
+#endif //HYPLAYER_FFMPEGENCODER_H

61afca3b19883fa7c7317068555d5369f1bd07a6
Wei Chen <weichen@narvii.com>
2019-09-25 18:47:44 +0800

    add filter

--- app/src/main/cpp/flutter/widget/ImageView.cpp
+++ app/src/main/cpp/flutter/widget/ImageView.cpp
@@ -4,6 +4,7 @@
 
 #include <core/SkData.h>
 #include <core/SkImage.h>
+#include <core/SkMaskFilter.h>
 #include "ImageView.h"
 
 void ImageView::onDraw(SkCanvas *canvas, int parentWidth, int parentHeight) {
@@ -15,5 +16,7 @@ void ImageView::onDraw(SkCanvas *canvas, int parentWidth, int parentHeight) {
     srcRect.setWH(image->width(), image->height());
     SkRect destRect;
     destRect.setLTRB(left * parentWidth, top * parentHeight, (width + left) * parentWidth, (top + height) * parentHeight);
+    sk_sp<SkMaskFilter> maskFilter = SkMaskFilter::MakeBlur(SkBlurStyle::kNormal_SkBlurStyle, 100.0);
+    paint.setMaskFilter(maskFilter);
     canvas->drawImageRect(image, srcRect, destRect, &paint);
 }

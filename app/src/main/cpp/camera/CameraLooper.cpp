6c50fd776918179f85c2c8b6d14f37f0340a35d6
Wei Chen <weichen@narvii.com>
2019-10-10 19:12:11 +0800

    gradle upgrade

---
+++ app/src/main/cpp/camera/CameraLooper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by templechen on 2019-10-09.
+//
+
+#include "CameraLooper.h"
---
+++ app/src/main/cpp/camera/CameraLooper.h
@@ -0,0 +1,18 @@
+//
+// Created by templechen on 2019-10-09.
+//
+
+#ifndef HYPLAYER_CAMERALOOPER_H
+#define HYPLAYER_CAMERALOOPER_H
+
+
+#include "../base/Looper.h"
+
+class CameraLooper : public Looper{
+
+public:
+
+};
+
+
+#endif //HYPLAYER_CAMERALOOPER_H

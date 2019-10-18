0b6a33e02451ed9f446ad3f5af306cafe1a53570
Wei Chen <weichen@narvii.com>
2019-10-18 10:37:35 +0800

    gradle upgrade

---
+++ app/src/main/cpp/player/VideoPlayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by templechen on 2019-10-18.
+//
+
+#include "VideoPlayer.h"
---
+++ app/src/main/cpp/player/VideoPlayer.h
@@ -0,0 +1,14 @@
+//
+// Created by templechen on 2019-10-18.
+//
+
+#ifndef HYPLAYER_VIDEOPLAYER_H
+#define HYPLAYER_VIDEOPLAYER_H
+
+
+class VideoPlayer {
+
+};
+
+
+#endif //HYPLAYER_VIDEOPLAYER_H
